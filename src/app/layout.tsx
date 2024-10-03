67c2f503cb646c5f62ab589187af2b95662e8e32
Adam Johnson <adam@adamjohnson.org>
2024-10-03 10:04:21 +1000

    Chronicle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,37 @@
 import type { Metadata } from "next";
+import localFont from "next/font/local";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Ripley - Local Favorites Curated by Elite Hotels",
-  description: "Coming to iOS and Android soon.",
+  description: "Coming soon to iOS and Android.",
 };
 
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
+});
+
+const chronicle = localFont({
+  src: [
+    {
+      path: "./fonts/Chronicle-Display-Roman.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Chronicle-Display-Italic.woff2",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "./fonts/Chronicle-Display-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-chronicle",
 });
 
 export default function RootLayout({
@@ -19,7 +41,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} font-sans`}>{children}</body>
+      <body className={`${chronicle.variable} font-serif, ${inter.variable} font-sans`}>{children}</body>
     </html>
   );
 }
